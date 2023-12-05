7397b6a1d2e7b734947d6218b43df52887c5bbfb
JRHuy <duchuy2001@outlook.com>
2023-12-05 15:22:41 +0700

    add codes to select 2 eligible offers and write outputs in output.json

--- app.js
+++ app.js
@@ -2,29 +2,53 @@ const fs = require('fs');
 const moment = require('moment');
 
 // get check-in date argument from the command line
-const checkinDate = '2019-12-25';
+const checkinDate = process.argv[2];
+// const checkinDate = '2019-12-25';
 
 // load data from input.json
-const data = JSON.parse(fs.readFileSync('input.json', 'utf8'));
+// const data = JSON.parse(fs.readFileSync('input.json', 'utf8'));
+fs.readFile('input.json', 'utf8', (err, data) => {
+    if (err) throw err;
 
-const categoryID = {
-    'Restaurant': 1,
-    'Retail': 2,
-    'Activity': 4
-};
+    const offers = JSON.parse(data).offers;
 
-let eligibleOffers = data.offers.filter(offer => {
-    // check if offer's category is in categoryID
-    if (Object.values(categoryID).includes(offer.category)) {
-        const validDate = moment(offer.valid_to, 'YYYY-MM-DD');
-        // check if offer is valid till check-in date + 5 days
-        if (validDate.isSameOrAfter(moment(checkinDate, 'YYYY-MM-DD').add(5, 'days'))) {
-            // if offer has multiple merchants, select the closest one
-            offer.merchants.sort((a, b) => a.distance - b.distance);
-            offer.merchants = [offer.merchants[0]];
-            return true;
+    const categoryID = {
+        'Restaurant': 1,
+        'Retail': 2,
+        'Activity': 4
+    };
+
+    let eligibleOffers = offers.filter(offer => {
+        // check if offer's category is in categoryID
+        if (Object.values(categoryID).includes(offer.category)) {
+            const validDate = moment(offer.valid_to, 'YYYY-MM-DD');
+            // check if offer is valid till check-in date + 5 days
+            if (validDate.isSameOrAfter(moment(checkinDate, 'YYYY-MM-DD').add(5, 'days'))) {
+                // if offer has multiple merchants, select the closest one
+                offer.merchants.sort((a, b) => a.distance - b.distance);
+                offer.merchants = [offer.merchants[0]];
+                return true;
+            }
+        }
+        return false;
+    });
+    // only return 2 offers even though there are several eligible offers
+    // If there are multiple offers in the same category give priority to the closest merchant offer.
+    // If there are multiple offers with different categories, select the closest merchant offers when selecting 2 offers
+    eligibleOffers.sort((a, b) => a.merchants[0].distance - b.merchants[0].distance);
+    finalOffers = [];
+    differentCategories = [];
+    for (let offer of eligibleOffers) {
+        if (!differentCategories.includes(offer.category)) {
+            finalOffers.push(offer);
+            differentCategories.push(offer.category);
+        }
+        if (finalOffers.length == 2) {
+            break;
         }
     }
-    return false;
-});
-console.log(eligibleOffers)
+
+    fs.writeFile('output.json', JSON.stringify({ offers: finalOffers }, null, 2), err => {
+        if (err) throw err;
+    })
+})
